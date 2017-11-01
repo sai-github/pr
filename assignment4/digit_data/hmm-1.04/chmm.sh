56d71ec37318cae3325ae16801085b6ff947bb80
sai <saiediitm@gmail.com>
2017-11-01 19:02:31 +0530

    modify.py script for modifying generated concatenated .hmm files and modified chmm.sh for printing sequence on termial

---
+++ assignment4/digit_data/hmm-1.04/chmm.sh
@@ -0,0 +1,13 @@
+python "chmms/modifiy.py"
+
+files=$( ls chmms/chmms_m/*.hmm )
+counter=0
+for i in $files
+do
+  let counter=$counter+1
+  ./test_hmm "d1_connected_test.out" $i
+  temp="$(cut -d'/' -f3 <<<"$i")"
+  cp alphaout "alphas_connected/"$temp".alpha"
+done
+
+python alphas_connected/pred_connected.py
--- assignment4/digit_data/hmm-1.04/chmms/modify.py
+++ assignment4/digit_data/hmm-1.04/chmms/modify.py
@@ -1,15 +1,36 @@
 import numpy as np
 import glob
 
-files = glob.glob('*')
+files = glob.glob('*_')
 
+file_lines = []
 for file in files:
-    lines = []
+    lines = None
     with open(file) as f:
-        lines.append(f.readlines())
-    nb_state = int(lines[0].strip().split(' ')[1])
-    nb_symbols = int(lines[1].strip().split(' ')[1])
+        lines = f.readlines()
+    file_lines.append(lines)
 
-    for data in lines[3:]:
+file_lines = np.array(file_lines)
 
-    
+file_count = 0
+for file in file_lines:
+    filename = 'chmms_m/'+files[file_count]+'.hmm'
+    file_count+=1
+    nb_state = int(file[0].strip().split(' ')[1])//3
+
+    for i in range(1,4):
+        state_end_index = 2+3*nb_state*i-1
+        l_temp = file[state_end_index-1].strip().split(' ') #19
+        l_temp[0] = '%.6f' %0.5
+        file[state_end_index-1] = ''.join(l_temp)+' \n'
+
+        r_temp = file[state_end_index].strip().split(' ') #20
+        r_temp[0] = '%.6f' %0.5
+        file[state_end_index] = ''.join(r_temp)+' \n'
+
+    line_val = 0
+    with open(filename, 'w') as f:
+        n_max = nb_state*3*3 +2 #3-rows in text file, 3-combinations
+        while(line_val<n_max):
+            f.write(file[line_val])
+            line_val+=1
